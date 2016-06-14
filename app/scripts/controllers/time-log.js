11d83d7e123cfdb6c40717fc4ea43b205653bc1c
Hazrul Mansor <HazrulMansor@wmgis.com>
2016-06-14 17:21:18 +0800

    firebase

--- app/scripts/controllers/time-log.js
+++ app/scripts/controllers/time-log.js
@@ -8,6 +8,23 @@
  * Controller of the timeLogApp
  */
 angular.module('timeLogApp')
-  .controller('TimeLogCtrl', ['$scope', 'firebase', function($scope, firebase) {
-    $scope.records = firebase.get();
-}]);
+  .controller('TimeLogCtrl', ['$scope', 'firebase', function ($scope, firebase) {
+
+    $scope.records = {};
+
+    firebase.getAllRecords().success(function (data) {
+      $scope.records = data;
+    }).error(function () {
+      alert('unable to pull time logs!!!');
+    });
+
+    $scope.saveRecord = function (record) {
+      firebase.saveRecord(record).success(function () {
+        alert('saved successfully!!!');
+      }).error(function () {
+        alert('something went wrong!!!');
+      });
+    };
+
+
+  }]);
--- app/scripts/services/firebase.js
+++ app/scripts/services/firebase.js
@@ -7,7 +7,32 @@
  * # firebase
  * Factory in the timeLogApp.
  */
-angular.module('timeLogApp')
-  .factory('firebase', function ($resource) {
-    return $resource('https://time-log.firebaseio.com/record.json');
-});
+angular.module('timeLogApp').service('firebase', ['$http', function ($http) {
+
+  this.getAllRecords = function () {
+    // return a Promise object so that the caller can handle success/failure
+    return $http({ method: 'GET', url: 'https://time-log.firebaseio.com/record.json' });
+  }
+
+  this.saveRecord = function (record) {
+    // return a Promise object so that the caller can handle success/failure
+    console.log(record);
+    var resourceUrl = "https://time-log.firebaseio.com/record/" + record.id + ".json";
+    return $http({ method: 'PUT', url: resourceUrl, record });
+  }
+
+  this.updateRecord = function (record) {
+    // return a Promise object so that the caller can handle success/failure
+    console.log(record);
+    var resourceUrl = "https://time-log.firebaseio.com/record/" + record.id + ".json";
+    return $http({ method: 'PATCH', url: resourceUrl });
+  }
+
+  this.deleteRecord = function (record) {
+    // return a Promise object so that the caller can handle success/failure
+    console.log(record);
+    var resourceUrl = "https://time-log.firebaseio.com/record/" + record.id + ".json";
+    return $http({ method: 'DELETE', url: resourceUrl });
+  }
+
+}]);
